bdc5e634c6b57050d0c3a73826e935977f1befbd
Ramin Dashti <ramin.dashti@hotmail.com>
2024-12-17 20:05:07 +0330

    applied relationship configs

--- Library-API/Persistence/Contexts/AppDbContext.cs
+++ Library-API/Persistence/Contexts/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Library_API.Models;
+using Library_API.Persistence.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Library_API.Persistence.Contexts
@@ -11,5 +12,11 @@ namespace Library_API.Persistence.Contexts
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+        }
     }
 }
